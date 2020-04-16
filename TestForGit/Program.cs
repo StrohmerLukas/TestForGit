7ea8985947c59f98df8368609221ff651e32cbdf
Lukas Strohmer <03StrLuk@rgtfo-me.it>
2020-04-16 15:32:26 +0200

    Projektdateien hinzufügen.

---
+++ TestForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
